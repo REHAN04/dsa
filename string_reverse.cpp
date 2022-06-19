5e32d861510a4dc8b7c6199750a803bd777f28f3
REHAN04 <rehan0404kumar@gmail.com>
2022-06-19 10:42:51 +0530

    Add files via upload

---
+++ string_reverse.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   string str;
+    getline(cin,str);
+    reverse(str.begin(),str.end());
+    cout<<str<<endl;
+    int i=-1,j=0;
+        while(j<str.length())
+        {
+            if(str[j]=='.')
+            {
+                reverse(str.begin()+i+1,str.begin()+j);
+                i=j;
+            }
+            j++;
+        }
+        //return S;
+        cout<<str<<endl;
+  return 0;
+}
+
